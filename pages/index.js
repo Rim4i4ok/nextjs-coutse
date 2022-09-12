75c121638d50445c6e0ae0cead4bb98ec8f3c5c7
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-12 23:02:31 +0300

    links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function HomePage() {
   return (
     <div>
       <h1>Home page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <a href="/portfolio">Portfolio (rerender)</a>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
