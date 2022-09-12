7067a5e4132ff000670303eca91107a2b96eb3a9
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-12 23:08:23 +0300

    dynamic links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "rim", name: "Rim4i4ok" },
+    { id: "bar", name: "Barlog" },
+    { id: "lem", name: "Lembas" },
+  ];
+
   return (
     <div>
       <h1>Clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
