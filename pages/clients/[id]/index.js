1755b0cf97c7f40547060b333dd350f830731a2b
Rim4i4ok <rim4i4ok@gmail.com>
2022-09-12 23:18:20 +0300

    program navigation

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,9 +5,26 @@ function ClientProjectsPage() {
 
   const { id } = router.query;
 
+  console.log(router.query);
+
+  function clientProjectHandler() {
+    // some hard work
+    router.push("/clients/max/someproject");
+  }
+
+  function clientProjectAltHandler() {
+    // some hard work
+    router.push({
+      pathname: "/clients/[id]/[clientProjectId]",
+      query: { id: "rim", clientProjectId: "someprojectalt" },
+    });
+  }
+
   return (
     <div>
       <h1>Client projects page with id: {id}</h1>
+      <button onClick={clientProjectHandler}>Load Project</button>
+      <button onClick={clientProjectAltHandler}>Load Project Alt</button>
     </div>
   );
 }
